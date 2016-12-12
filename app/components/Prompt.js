f81d5611a0fce03ab6a27a73747fdea9bf8c3aeb
gyver98 <matthewchoi7@gmail.com>
2016-12-13 10:45:11 +1100

    stateless functional component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -2,25 +2,18 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var transparentBg = require('../styles').transparentBg;
 
-var Prompt = React.createClass({
-    propTypes: {
-        header: PropTypes.string.isRequired,
-        onUpdateUser: PropTypes.func.isRequired,
-        onSubmitUser: PropTypes.func.isRequired,
-        username: PropTypes.string.isRequired
-    },
-    render: function () {
-        return (
-          <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-              <h1>{this.props.header}</h1>
+function Prompt (props) {
+    return (
+            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+              <h1>{props.header}</h1>
               <div className="col-sm-12">
-                <form onSubmit={this.props.onSubmitUser}>
+                <form onSubmit={props.onSubmitUser}>
                   <div className="form-group">
                     <input 
                       className="form-control"
                       placeholder="Github Username"
-                      onChange={this.props.onUpdateUser}
-                      value={this.props.username}
+                      onChange={props.onUpdateUser}
+                      value={props.username}
                       type="text"
                     />
                   </div>
@@ -34,8 +27,14 @@ var Prompt = React.createClass({
                 </form>
               </div>
             </div>    
-        )
-    }
-});
+    )
+}
+Prompt.propTypes = {
+    header: PropTypes.string.isRequired,
+    onUpdateUser: PropTypes.func.isRequired,
+    onSubmitUser: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired 
+}
+
 
 module.exports = Prompt;
