8109d911d90da1aa18f65dd5921dafa1222fedd8
gyver98 <matthewchoi7@gmail.com>
2016-12-13 14:19:33 +1100

    Axios, Promises, and the Githup API

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes: {
@@ -17,8 +18,15 @@ var ConfirmBattleContainer = React.createClass({
     },
     componentDidMount: function () {
         var query = this.props.location.query;
-        console.log('componentDidMount');
+        
         // Fetch info from github then update state
+        githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+          .then(function (players) {
+              this.setState({
+                  isLoading: false,
+                  playerInfo: [players[0], players[1]]
+              })
+          }.bind(this));
     },
     componentWillReceiveProps: function () {
         console.log('componentWillReceieveProps');
