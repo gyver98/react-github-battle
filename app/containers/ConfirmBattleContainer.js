1ece6c6466f963886ab652f23af4abe1bce62735
gyver98 <matthewchoi7@gmail.com>
2016-12-13 12:58:10 +1100

    React life cycle event and conditional rendering

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,38 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function () {
+        console.log('getInitialState');
+        return {
+            isLoading: true,
+            playerInfo: []
+        }
+    },
+    componentWillMount: function () {
+        console.log('componentWillMount');
+    },
+    componentDidMount: function () {
+        var query = this.props.location.query;
+        console.log('componentDidMount');
+        // Fetch info from github then update state
+    },
+    componentWillReceiveProps: function () {
+        console.log('componentWillReceieveProps');
+    },
+    componentWillUnmount: function () {
+        console.log('componentWillUnMount')
+    },
+    render: function () {
+        return (
+            <ConfirmBattle 
+              isLoading={this.state.isLoading}
+              playerInfo={this.state.playerInfo}/>
+        );
+    }
+});
+
+module.exports = ConfirmBattleContainer;
